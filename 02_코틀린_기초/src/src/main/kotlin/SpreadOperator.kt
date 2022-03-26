3243b8b55eec3a43789101b438c0d3b100499116
BAEKJungHo <designjava@naver.com>
2022-03-26 16:14:02 +0900

    Spread Operator

---
+++ 02_코틀린_기초/src/src/main/kotlin/SpreadOperator.kt
@@ -0,0 +1,14 @@
+/** Spread Operator
+ * 이미 배열에 들어있는 원소를 가변 길이의 인자로 넘길 때, 배열의 원소를 명시적으로 풀어서 인자로 전달되게 해야 한다.
+ */
+
+/*
+fun listOf<T>(vararg values: T) : List<T> {
+    // ...
+}
+fun main(args: Array<String>) {
+    val list = listOf("args : ", *args)
+    println(list)
+}
+*/
+
